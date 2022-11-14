689788e5345959267f56fb28015a6cd901221cbf
ritamirgorodska <ritamirgorodska.1604@gmail.com>
2022-11-14 21:04:47 +0300

    edit task 5

--- js/task-05.js
+++ js/task-05.js
@@ -5,7 +5,16 @@ const refs = {
 
 refs.input.addEventListener("input", onInputChange);
 
+// function onInputChange(event) {
+//   console.log(event.currentTarget.value);
+//   refs.nameLabel.textContent = event.currentTarget.value;
+//   if (event.currentTarget.value === "") {
+//     return "Anonymous";
+//   }
+// }
 function onInputChange(event) {
-  console.log(event.currentTarget.value);
   refs.nameLabel.textContent = event.currentTarget.value;
+  if (!refs.nameLabel.textContent) {
+    return (refs.nameLabel.textContent = "Anonymous");
+  }
 }
